a89c314dd69b99b262003df39495f7b4ae0ee3e9
Przemek Ciąćka <przemek@ciacka.net>
2018-11-27 22:23:33 +0100

    Validate request body.

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 const serverless = require('serverless-http');
 const express = require('express');
 const app = express();
+const { check, validationResult } = require('express-validator/check');
 
-app.post('/message', function (req, res) {
-    res.send('Hello World!')
+app.use(express.json());
+app.post('/message', [
+    check('name').isLength({ min: 3 }),
+    check('email').isEmail(),
+    check('subject').optional().isString(),
+    check('message').isLength({ min: 10 })
+], (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(422).json({ errors: errors.array() });
+    }
+
+    return res.send('OK');
 });
 
 module.exports.handler = serverless(app);
