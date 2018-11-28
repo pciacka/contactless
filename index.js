b21d067dd77aebdb325b528efe51e596411c57c3
Przemek Ciąćka <przemek@ciacka.net>
2018-11-28 12:47:07 +0100

    Create .env.example file.

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
+// Load .env file
+require('dotenv').config();
+
 const serverless = require('serverless-http');
 const express = require('express');
 const app = express();
+const AWS = require('aws-sdk');
+const awsSES = new AWS.SES();
+
 const { check, validationResult } = require('express-validator/check');
 
 app.use(express.json());
@@ -15,7 +21,27 @@ app.post('/message', [
         return res.status(422).json({ errors: errors.array() });
     }
 
-    return res.send('OK');
+    const subject = req.body.subject ? req.body.subject : 'Wiadomość ze strony ciacka.net';
+    const message = `${req.body.message}\n\n--\n${req.body.name}\n\nWiadomość wysłana ze strony ciacka.net`;
+
+    const sesParams = {
+        Destination: { ToAddresses: ['receiver@example.com'] },
+        Message: {
+            Body: {
+                Text: { Data: message, Charset: 'UTF-8' }
+            },
+            Subject: { Data: subject, Charset: 'UTF-8' }
+        },
+        Source: 'sender@example.com',
+        ReplyToAddresses: [req.body.email]
+    };
+
+    awsSES.sendEmail(sesParams).promise().then(() => {
+        return res.status(204);
+    }).catch((err) => {
+        console.log(err, err.stack);
+        return res.status(500).json({status: 'FAILURE', message: 'Internal server error occured.'});
+    });
 });
 
 module.exports.handler = serverless(app);
